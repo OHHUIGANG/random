419a1db722263903d5fa1b7bb136f357c9e855dd
OHHUIGANG <rntlqpgmlrkd@naver.com>
2020-07-26 00:10:00 +0900

    드디어 했다 ㅠㅠ

---
+++ flutter_please/lib/src/random_list.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _RandomListState();
+  }
+}
+
+class _RandomListState extends State<RandomList> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('naming app'),
+      ),
+      body: _buildList(),
+    );
+  }
+
+  Widget _buildList(){
+    return ListView.builder(itemBuilder: (context, index) {
+      if (index.isOdd) {
+      return Divider();
+    }
+      var realIndex = index ~/ 2;
+
+      if (realIndex >= _suggestions.length) {
+      _suggestions.addAll(generateWordPairs().take(10));
+      }
+
+      return _buildRow(_suggestions[realIndex]);
+    });
+  }
+
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          textScaleFactor: 2,
+        ));
+  }
+}
